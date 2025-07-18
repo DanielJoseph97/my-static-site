3b1b06ea4fdbb424b229a20018f41e551e2d5049
DanielJoseph97 <danjoseph97@gmail.com>
2025-07-18 06:30:33 +0000

    Complete split images and url for inline markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -5,15 +5,15 @@ import re
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
 # old nodes in this case are nodes with compound text types that have to split up
 # this function will split up the old nodes and return a new node with all the text types split up
-    new_node = []
+    new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
-            new_node.append(old_node)
+            new_nodes.append(old_node)
             continue
         split_nodes = old_node.text.split(delimiter)
         if len(split_nodes) == 1:
             # This means delimiter is not found and string returns as is
-            new_node.append(old_node)
+            new_nodes.append(old_node)
         elif len(split_nodes) % 2 == 0:
             # Odd number delimiter or missing a closing delimiter
             raise Exception(" invalid markdown syntax: no closing delimiter found")
@@ -24,11 +24,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     # Even index (regular text or empty string)
                     if split_nodes[i] == "":
                         continue
-                    new_node.append(TextNode(split_nodes[i], TextType.TEXT))
+                    new_nodes.append(TextNode(split_nodes[i], TextType.TEXT))
                 else:
                     # Odd index is delimited text
-                    new_node.append(TextNode(split_nodes[i], text_type))
-    return new_node
+                    new_nodes.append(TextNode(split_nodes[i], text_type))
+    return new_nodes
 
 def extract_markdown_images(text):
     result = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
@@ -39,31 +39,50 @@ def extract_markdown_links(text):
     return result
 
 def split_nodes_image(old_nodes):
-    new_node = []
+    new_nodes = []
     for node in old_nodes:
-        image_props = extract_markdown_images(node.text) #list of image alt and links
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        current_text = node.text
+        image_props = extract_markdown_images(current_text) #list of image alt and links
         if image_props == []:
             #No image alt or links found
-            new_node.append(node)
-        else:
-            for image_alt, image_link in list(image_props):
-                section = node.text.split(f"![{image_alt}]({image_link})", 1)
-                new_node.append(TextNode(section[0],TextType.TEXT))
-                new_node.append(TextNode(image_alt, TextType.IMAGE, image_link))
-                new_node.append(TextNode(section[1],TextType.TEXT))            
-    return new_node
+            new_nodes.append(node)
+            continue
+        for image_alt, image_link in image_props:
+            section = current_text.split(f"![{image_alt}]({image_link})", 1)
+            if len(section) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if section[0] != "":
+                new_nodes.append(TextNode(section[0], TextType.TEXT))
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE,image_link))
+            current_text = section[1]
+        if current_text != "":
+            new_nodes.append(TextNode(current_text,TextType.TEXT))          
+    return new_nodes
 
 
 def split_nodes_link(old_nodes):
-    new_node = []
+    new_nodes = []
     for node in old_nodes:
-        image_props = extract_markdown_links(node.text) #list of image alt and links
-        if len(image_props) == 1:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        current_text = node.text
+        link_props = extract_markdown_links(current_text) #list of image alt and links
+        if link_props == []:
             #No image alt or links found
-            new_node.append(node)
-        else:
-            for link_alt, link_url in list(image_props):
-                section = node.text.split(f"![{link_alt}]({link_url})", 1)
-                new_node.append(TextNode(section[0],TextType.TEXT))
-                new_node.append(tuple(link_alt, link_url))
-    return new_node
+            new_nodes.append(node)
+            continue
+        for link_alt, link_url in link_props:
+            section = current_text.split(f"[{link_alt}]({link_url})", 1)
+            if len(section) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if section[0] != "":
+                new_nodes.append(TextNode(section[0], TextType.TEXT))
+            new_nodes.append(TextNode(link_alt, TextType.LINK,link_url))
+            current_text = section[1]
+        if current_text != "":
+            new_nodes.append(TextNode(current_text,TextType.TEXT))          
+    return new_nodes

f18b2792c628c1a3fc649c67bbd3de38e297f4df
DanielJoseph97 <danjoseph97@gmail.com>
2025-07-18 08:45:45 +0000

    Add block to block type functions and unittests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -7,3 +17,21 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+def block_to_block_type(block):
+    headings = ["# ","## ","### ","#### ","##### ","###### "]
+    if any(block.startswith(h) for h in headings):
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    lines = block.split("\n")
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    if all(line.startswith(f"{idx+1}. ") for idx, line in enumerate(lines)):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+    
+    
+    
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,6 +45,59 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_headings(self):
+        input1 = "# This is a heading"
+        input2 = "## This is a heading"
+        input3 = "### This is a heading"
+        input4 = "#### This is a heading"
+        input5 = "##### This is a heading"
+        input6 = "###### This is a heading"
+        block_1 = block_to_block_type(input1)
+        block_2 = block_to_block_type(input2)
+        block_3 = block_to_block_type(input3)
+        block_4 = block_to_block_type(input4)
+        block_5 = block_to_block_type(input5)
+        block_6 = block_to_block_type(input6)
+        self.assertEqual(block_1, BlockType.HEADING)
+        self.assertEqual(block_2, BlockType.HEADING)
+        self.assertEqual(block_3, BlockType.HEADING)
+        self.assertEqual(block_4, BlockType.HEADING)
+        self.assertEqual(block_5, BlockType.HEADING)
+        self.assertEqual(block_6, BlockType.HEADING)
+    
+    def test_code(self):
+        input = "```\nsome code\n```"
+        block = block_to_block_type(input)
+        self.assertEqual(block, BlockType.CODE)
+    
+    def test_quote(self):
+        input = "> To be\n> or not to be"
+        block = block_to_block_type(input)
+        self.assertEqual(block,BlockType.QUOTE)
+    
+    def test_unordered_list(self):
+        input = "- some stuff\n- some more stuff\n- even more stuff"
+        block = block_to_block_type(input)
+        self.assertEqual(block,BlockType.UNORDERED_LIST)
+
+    def test_ordered_list(self):
+        input = "1. some stuff\n2. some more stuff\n3. even more stuff"
+        block = block_to_block_type(input)
+        self.assertEqual(block,BlockType.ORDERED_LIST)
+    
+    def test_paragraph(self):
+        input = "Some text that shoul" \
+        "d fall in between all scenari" \
+        "os\n- such that no single bloc" \
+        "k type is detected\n1. so hopeful" \
+        "ly\nthis works as intended\n> A" \
+        "s they say\ncan only test somethi" \
+        "ng\n```by trying it out```\nso her" \
+        "e's an attempt"
+        block = block_to_block_type(input)
+        self.assertEqual(block, BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
